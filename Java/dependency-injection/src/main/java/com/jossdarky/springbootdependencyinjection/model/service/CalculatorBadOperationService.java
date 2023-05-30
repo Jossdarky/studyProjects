0eb6708c77e587fd17e01b322c4b588fee6ff3ed
JosePalomino <jose.palomino@fouriering.com>
2023-05-30 17:23:53 -0500

    dependency injection creation project

---
+++ Java/dependency-injection/src/main/java/com/jossdarky/springbootdependencyinjection/model/service/CalculatorBadOperationService.java
@@ -0,0 +1,30 @@
+package com.jossdarky.springbootdependencyinjection.model.service;
+
+import java.util.List;
+
+public class CalculatorBadOperationService implements ICalculatorService{
+
+    @Override
+    public double addition(List<Double> numbers) {
+        return 4.0;
+    }
+
+    @Override
+    public double subtraction(List<Double> numbers) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'subtraction'");
+    }
+
+    @Override
+    public double multiplication(List<Double> numbers) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'multiplication'");
+    }
+
+    @Override
+    public double division(List<Double> numbers) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'division'");
+    }
+    
+}
